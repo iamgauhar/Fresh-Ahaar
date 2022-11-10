3fbaefa942519657cc24755d2cbaff1a48753850
Pannakbhushana <rahulkumarmishra5338@gmail.com>
2022-11-10 10:36:53 +0530

    work in progress

--- KindMoments/kindMoments.js
+++ KindMoments/kindMoments.js
@@ -3,3 +3,13 @@ import { header } from "../components/utility.js";
 
 let navbar=document.getElementById('navbar');
 navbar.innerHTML=header();
+
+const createBtn= ()=>{
+
+    for(let i=1; i<=10; i++){
+        let btn=document.createElement('button');
+        btn.innerText=i;
+        document.getElementById('btn').append(btn)
+    }
+}
+createBtn()
